37c83057383b78163ad58b27bd9a65df557e9505
Carla Macia Diez <shiryukocontact@gmail.com>
2026-07-06 16:25:29 +0200

    Added social links components to the Footer

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,4 +5,11 @@ export const site = {
 		{ label: 'Experience', href: '/#experience' },
 		{ label: 'Projects', href: '/#projects' },
 	],
+	social: [
+		{ label: 'GitHub', href: 'https://github.com/shiryuko', icon: 'github' },
+		{ label: 'LinkedIn', href: 'https://google.com', icon: 'linkedin' },
+	],
 } as const;
+
+export type SocialIconName = (typeof site.social)[number]['icon'];
+export type SocialLink = (typeof site.social)[number];
